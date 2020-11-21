3866030162601903c8009f6ee153e9dcfd01c55d
billyott <willrichott@gmail.com>
2020-11-21 15:16:00 -0500

    Done

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,42 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+  
+  state = {
+    pokemons: [],
+    searchTerm: ''
+  }
+  
+  componentDidMount() {
+    fetch('http://localhost:3000/pokemon')
+    .then(resp => resp.json())
+    .then(pokemons => {
+      this.setState({pokemons: pokemons})
+    })
+  }
+
+  filteredPokemon = () => {
+    return [...this.state.pokemons].filter(pokemon => pokemon.name.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
+  }
+
+  handleFilter = (searchTerm) => {
+    this.setState({searchTerm: searchTerm})
+  }
+
+  handleSubmit = (newPokemon) => {
+    this.setState({pokemons: [...this.state.pokemons, newPokemon]})
+  }
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm handleSubmit={this.handleSubmit}/>
         <br />
-        <Search />
+        <Search handleFilter={this.handleFilter} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={this.filteredPokemon()}/>
       </Container>
     )
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,26 @@
 import React from 'react'
 
-const Search = props => {
-  return (
-    <div className="ui search">
-      <div className="ui icon input">
-        <input className="prompt"/>
-        <i className="search icon" />
+class Search extends React.Component {
+
+  state = {
+    name: ''
+  }
+
+  localHandleFilter = (e) => {
+    this.props.handleFilter(e.target.value)
+    this.setState({name: e.target.value})
+  }
+
+  render() {
+    return (
+      <div className="ui search">
+        <div className="ui icon input">
+          <input className="prompt" value={this.state.searchTerm} onChange={this.localHandleFilter}/>
+          <i className="search icon" />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Search
